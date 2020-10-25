e0ac81bd819352ac08bdb78abce3cce21eaeca28
nmpiovano@hotmail.com <nmpiovano@hotmail.com>
2020-10-25 19:58:02 -0300

    log

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -15,58 +15,68 @@ IMPORTANTE REQ.PARAMS PARA OBTENER IDS PASADOS POR PARMETRO. REQ.BODY/REQ.QUERY 
 //Se obtiene con req.body
 //Se obtiene con req.query EN HEROKU
 router.get('/signUp' , (req, res) => {
-	User.find({"email": req.query.email}).then(user=>{ 
-		//hay un registro
-		if(user.length > 0){
-			res.json({
-				existeUsuario: true,
-				color: 'red',
-				mensaje: 'Existe ya un usuario con ese mail'
-			})
-		} else {
-			const newUser = new User(req.query)
-			newUser.save().then(()=>
-				res.json({
-					existeUsuario: false,
-					color: 'green',
-					mensaje: 'Usuario creado!'
-				})
-			)
-		}
+	let params = req.query
+	params.length < 1 ? params = req.body : null
+	res.json({
+		a: params
 	})
+	// User.find({"email": params.email}).then(user=>{ 
+	// 	//hay un registro
+	// 	if(user.length > 0){
+	// 		res.json({
+	// 			existeUsuario: true,
+	// 			color: 'red',
+	// 			mensaje: 'Existe ya un usuario con ese mail'
+	// 		})
+	// 	} else {
+	// 		const newUser = new User(params)
+	// 		newUser.save().then(()=>
+	// 			res.json({
+	// 				existeUsuario: false,
+	// 				color: 'green',
+	// 				mensaje: 'Usuario creado!'
+	// 			})
+	// 		)
+	// 	}
+	// })
 }) 
 
 
 //Se obtiene con req.body EN LOCAL
 //Se obtiene con req.query EN HEROKU
 router.get('/signIn' , (req, res) => { 
-	User.find({"email": req.query.email, "password": req.query.password}).then(user=>{ 
-		// hay usuario
-		if (user.length > 0) {
-			res.json({
-				error: false
-			})
-		} else {
-			//no hay pero existe el mail
-			User.find({"email": req.query.email}).then(user=>{ 
-				console.log(user)
-				if (user.length > 0) {
-					res.json({
-						error: true,
-						color: 'red',
-						mensaje: 'Contraseña incorrecta.'
-					})
-				} else {
-					res.json({
-						error: true,
-						color: 'red',
-						mensaje: 'Credenciales incorrectas. Logueate, por favor.'
-					})
-				}
-			})
-
-		}
+	let params = req.query
+	params.length < 1 ? params = req.body : null
+	res.json({
+		a: params
 	})
+	// User.find({"email": params.email, "password": params.password}).then(user=>{ 
+	// 	// hay usuario
+	// 	if (user.length > 0) {
+	// 		res.json({
+	// 			error: false
+	// 		})
+	// 	} else {
+	// 		//no hay pero existe el mail
+	// 		User.find({"email": params.email}).then(user=>{ 
+	// 			console.log(user)
+	// 			if (user.length > 0) {
+	// 				res.json({
+	// 					error: true,
+	// 					color: 'red',
+	// 					mensaje: 'Contraseña incorrecta.'
+	// 				})
+	// 			} else {
+	// 				res.json({
+	// 					error: true,
+	// 					color: 'red',
+	// 					mensaje: 'Credenciales incorrectas. Logueate, por favor.'
+	// 				})
+	// 			}
+	// 		})
+
+	// 	}
+	// })
 }) 
 
 
